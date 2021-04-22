2c9fb4bd9c1c0a8bcfc9d7bf558c089c1ef0dfdc
darae07 <leedr0730@gmail.com>
2021-04-23 01:19:22 +0900

    tutorial 4. test code

--- placebook/polls/views.py
+++ placebook/polls/views.py
@@ -5,13 +5,14 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Question, Choice
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # template = loader.get_template('polls/index.html')
     # context = {
@@ -23,6 +24,8 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
     # question = get_object_or_404(Question, pk=question_id)
     # return render(request, 'polls/detail.html', {'question': question})
 

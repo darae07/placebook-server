9bd4a7a0039921fc073ded602760234d84ded198
darae07 <leedr0730@gmail.com>
2021-05-05 18:36:07 +0900

    지역 정보 테이블 생성

--- placebook/crwalingTutorial/models.py
+++ placebook/crwalingTutorial/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import pandas
 
 # Create your models here.
+df = pandas.read_csv('sample.csv')
+df = df[['dong']]
+# df.duplicated(['dong'])
+converted = df.drop_duplicates(subset=['dong'], ignore_index = True, keep='first')
+converted.to_csv('dong_unique.csv')
+
